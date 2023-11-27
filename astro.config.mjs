c761bdcf248993db19bad60bf36c92fdf2f67d32
eduardodgdebem <bemeduardode@gmail.com>
2023-11-27 15:12:26 +0100

    add vercel suport

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import dotenv from 'dotenv';
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 dotenv.config();
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  output: "server",
+  adapter: vercel()
 });
